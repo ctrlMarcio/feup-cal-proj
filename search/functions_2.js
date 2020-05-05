7c8b92b255721ccbafb8f070551f77530f67ae8e
ctrlMarcio <zmduartez@gmail.com>
2020-05-05 12:47:22 +0000

    Deploying to gh-pages from  @ e75f3873b48f5ff1451e77497b1475a7eb88ec11 🚀

--- search/functions_2.js
+++ search/functions_2.js
@@ -4,6 +4,5 @@ var searchData=
   ['getdest',['getDest',['../classEdge.html#a29f8741fd300c40d3996c16853b6e509',1,'Edge']]],
   ['getinfo',['getInfo',['../classVertex.html#a48eae2f7af2362634adab02b7b2dbec6',1,'Vertex']]],
   ['getvertexset',['getVertexSet',['../classGraph.html#ab676121837045f14d2f0c3e6fad2c1c0',1,'Graph']]],
-  ['getweight',['getWeight',['../classEdge.html#a3df378e283d6c8be5be4170ac8d7f4e8',1,'Edge']]],
-  ['graph',['Graph',['../classGraph.html',1,'']]]
+  ['getweight',['getWeight',['../classEdge.html#a3df378e283d6c8be5be4170ac8d7f4e8',1,'Edge']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,18 +1,21 @@
 var indexSectionsWithContent =
 {
-  0: "cegv",
-  1: "cegv"
+  0: "acegv",
+  1: "cegv",
+  2: "aegv"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes"
+  1: "classes",
+  2: "functions"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Classes"
+  1: "Classes",
+  2: "Functions"
 };
 
