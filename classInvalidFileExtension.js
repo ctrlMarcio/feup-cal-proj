11931c43240ba663b8ad12bb2f36c5a74572e060
ctrlMarcio <zmduartez@gmail.com>
2020-05-08 08:42:00 +0000

    Deploying to gh-pages from  @ 72e4bba980e60a34bb971421af9daa1259503ea7 🚀

---
+++ classInvalidFileExtension.js
@@ -0,0 +1,4 @@
+var classInvalidFileExtension =
+[
+    [ "InvalidFileExtension", "classInvalidFileExtension.html#aaf78a1b891bc2063cabec24703e7c130", null ]
+];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['invalidfileextension',['InvalidFileExtension',['../classInvalidFileExtension.html',1,'']]],
   ['invalidloginexception',['InvalidLoginException',['../classInvalidLoginException.html',1,'']]]
 ];
