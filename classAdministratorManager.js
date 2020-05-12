daff7a1e99e62ec5d8637ac469a83df346bffaa1
pdff2000 <50109981+pdff2000@users.noreply.github.com>
2020-05-12 15:35:08 +0000

    Deploying to gh-pages from  @ 89e6783b3d320001c8a64948356beecdcd44e2d6 🚀

--- classAdministratorManager.js
+++ classAdministratorManager.js
@@ -2,6 +2,7 @@ var classAdministratorManager =
 [
     [ "AdministratorManager", "classAdministratorManager.html#a172ee105335c3a95eae921f7b399190b", null ],
     [ "add", "classAdministratorManager.html#af266faa5205b8b3750fa84f898f3bfba", null ],
+    [ "getAdministratorSet", "classAdministratorManager.html#a1ed1b75504a4120fa63971d573bd5104", null ],
     [ "isValid", "classAdministratorManager.html#acf048648d07e84c3f566df334bc2b1d6", null ],
     [ "remove", "classAdministratorManager.html#a9c1a97979dab0d729eccd801abbdcc78", null ]
 ];
--- classCompanyClientManager.js
+++ classCompanyClientManager.js
@@ -2,6 +2,7 @@ var classCompanyClientManager =
 [
     [ "CompanyClientManager", "classCompanyClientManager.html#a607c18a5e2033dad0fb4be216965fa0e", null ],
     [ "add", "classCompanyClientManager.html#aebc66a2763845de374825ab529a2e60d", null ],
+    [ "getCompanyClientSet", "classCompanyClientManager.html#acd489d99314809f1b225cc2da2dfa5a6", null ],
     [ "isValid", "classCompanyClientManager.html#ac715fee48eed3c636e709415437decd1", null ],
     [ "remove", "classCompanyClientManager.html#a613d5b950cc2724326a5facb689a7f36", null ]
 ];
